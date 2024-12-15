b3e87f1823bef105818dbca3105dcaae4b246e54
mumtazharis <ahmadmumtazhrs@gmail.com>
2024-12-15 22:09:19 +0700

    contactus

--- api/contact_us.py
+++ api/contact_us.py
@@ -1,13 +1,15 @@
 from flask import jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from config import db
 from models import ContactUs
 
 def contact_admin():
+    user_id = get_jwt_identity()
     data = request.get_json()
     new_message = ContactUs(
-        user_id=data['user_id'],
+        user_id=user_id,
         message=data['message']
     )
     db.session.add(new_message)
     db.session.commit()
-    return jsonify({"message": "Message sent"}), 201
+    return jsonify({"message": "Pesan berhasil terkirim"}), 201
